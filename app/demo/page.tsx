e2f71fef56fce203700a028dddc6307b663a38ff
yaoweijian <rubygba@foxmail.com>
2023-06-21 10:04:03 +0800

    feat: use app router

---
+++ app/demo/page.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: yaoweijian rubygba@foxmail.com
+ * @Date: 2023-06-21 09:52:29
+ * @LastEditors: yaoweijian rubygba@foxmail.com
+ * @LastEditTime: 2023-06-21 09:53:23
+ * @FilePath: /nextjs-react/app/demo/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <div className="prose dark:prose-invert">
+      <Link href="#">{'aaa'}</Link>
+    </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+/*
+ * @Author: yaoweijian rubygba@foxmail.com
+ * @Date: 2023-05-27 09:15:16
+ * @LastEditors: yaoweijian rubygba@foxmail.com
+ * @LastEditTime: 2023-06-21 09:53:59
+ * @FilePath: /nextjs-react/next.config.js
+ * @Description: init
+ */
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 module.exports = nextConfig
